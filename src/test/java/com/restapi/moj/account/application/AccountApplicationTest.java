3938c3285ef57e7673c6409177d90eb407167d1b
Sivagopal Balivada <sivagopal.b@gmail.com>
2018-09-05 01:12:37 +0100

    Controller tests created

--- src/test/java/com/restapi/moj/account/application/AccountApplicationTest.java
+++ src/test/java/com/restapi/moj/account/application/AccountApplicationTest.java
@@ -1,9 +1,11 @@
 package com.restapi.moj.account.application;
 
+import com.restapi.moj.account.application.com.restapi.moj.account.application.service.AccountService;
 import com.restapi.moj.account.application.controller.AccountController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,8 +18,12 @@ public class AccountApplicationTest {
     @Autowired
     private AccountController controller;
 
+    @Autowired
+    private AccountService accountService;
+
     @Test
     public void loadContext() {
         assertThat(controller).isNotNull();
+        assertThat(accountService).isNotNull();
     }
 }
---
+++ src/test/java/com/restapi/moj/account/application/com/restapi/moj/account/application/controller/AccountControllerTest.java
@@ -0,0 +1,43 @@
+package com.restapi.moj.account.application.com.restapi.moj.account.application.controller;
+
+import com.google.gson.Gson;
+import com.restapi.moj.account.application.AccountSpringBootApplication;
+import com.restapi.moj.account.application.com.restapi.moj.account.application.service.AccountService;
+import com.restapi.moj.account.application.data.Account;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = AccountSpringBootApplication.class)
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+public class AccountControllerTest {
+    @Autowired
+    private MockMvc accountMockMvc;
+
+    @MockBean
+    private AccountService accountService;
+
+    @Test
+    public void givenSavedAccounts_whenGetAccounts_theReturnAccountsAsJsonArray() throws Exception {
+        Account account = new Account("test", "test second", "123");
+        Gson gson = new Gson();
+        String jsonAccount = gson.toJson(account);
+        accountMockMvc.perform(post("/rest/account/json")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonAccount))
+                .andExpect(status().isOk());
+
+    }
+}
