a072bcb46f2da6e2411e2c17e6bedc34b0a6bc99
sivabalivada <sivagopal.b@gmail.com>
2018-09-30 17:36:21 +0100

    Swagger documentation added

--- src/main/java/com/restapi/moj/account/application/controller/AccountController.java
+++ src/main/java/com/restapi/moj/account/application/controller/AccountController.java
@@ -5,6 +5,7 @@ import com.restapi.moj.account.application.data.Account;
 import com.restapi.moj.account.application.response.AccountMessage;
 import com.restapi.moj.account.application.service.AccountService;
 import com.restapi.moj.account.application.util.JsonSchemaValidator;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +15,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 import javax.validation.Valid;
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @Controller
+@Api(value="AccountsController", produces = APPLICATION_JSON_VALUE)
 public class AccountController {
     @Autowired
     private AccountService accountService;
 
     @RequestMapping(value="/rest/account/json", method = RequestMethod.POST, produces = APPLICATION_JSON_VALUE)
+    @ApiOperation("Saves account information")
+    @ApiResponses(value={@ApiResponse(code=200, message = "OK", response = AccountMessage.class)})
     public @ResponseBody AccountMessage saveAccount(@Valid @RequestBody JsonNode accountNode) {
         try {
             if (new JsonSchemaValidator().isValid("/saveAccount.json", accountNode)) {
@@ -42,6 +47,8 @@ public class AccountController {
     }
 
     @RequestMapping(value="/rest/account/json/{id}", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+    @ApiOperation("Gets the account information for specific account id")
+    @ApiResponses(value={@ApiResponse(code=200, message = "OK", response = Account.class)})
     public @ResponseBody Account getAccountById(@PathVariable long id) {
         try {
 
@@ -55,6 +62,8 @@ public class AccountController {
     }
 
     @RequestMapping(value="/rest/account/json", method = RequestMethod.GET, produces = APPLICATION_JSON_VALUE)
+    @ApiOperation("Gets all available accounts")
+    @ApiResponses(value={@ApiResponse(code=200, message = "OK", response = List.class)})
     public @ResponseBody List<Account> getAccounts() {
             return accountService.getAllAccounts();
     }
@@ -68,8 +77,5 @@ public class AccountController {
             accountMessage.setMessage("Unable to delete account");
             return accountMessage;
         }
-
     }
-
-
 }
